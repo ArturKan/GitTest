8d9f421c17597c3c176666bef8a8f806516a15d5
Bjorn <515-1@mail.ru>
2022-11-08 10:10:01 +0300

    New changes

--- src/script.js
+++ src/script.js
@@ -2,6 +2,13 @@ function helloGitty() {
 	alert('Hello from Git!');
 }
 
-function sayBye() {
-	alert('Goodbye from Git!')
+function work() {
+alert('Work, work!');
+//...work in progress...
 }
+
+function sayBye() {
+	alert('Goodbye from Git!');
+}
+
+//....more changes here too...
